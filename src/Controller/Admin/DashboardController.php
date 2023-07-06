eab523712ca172e16a1e63eed91e71e537708df5
chambonnet christopher <chris80780@yahoo.fr>
2023-07-06 13:05:55 +0200

    com5

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Controller\CategorieController;
+use App\Controller\PlatController;
+use App\Entity\Categorie;
+use App\Entity\Plat;
+use App\Entity\Commande;
+use App\Entity\Detail;
+use App\Entity\Utilisateur;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    public function __construct(
+        private AdminUrlGenerator $adminUrlGenerator
+    ) {
+    }
+
+    #[Route('/admin', name: 'app_admin')]
+    public function index(): Response
+    {
+
+        $url = $this->adminUrlGenerator
+            ->setController(PlatCrudController::class)
+            // ->setController(CategorieController::class)
+            ->generateUrl();
+
+        return $this->redirect($url);
+
+        // return $this->render('Admin/admin.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Symfony District');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::section('Catégories');
+
+        yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('Voir les catégories', 'fas fa-eye', Categorie::class),
+            MenuItem::linkToCrud('Ajouter une catégorie', 'fas fa-plus', Categorie::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Modifier une catégorie', 'fas fa-plus', Categorie::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Supprimer une catégorie', 'fas fa-plus', Categorie::class)->setAction(Crud::PAGE_NEW),
+        ]);
+
+        yield MenuItem::section('Plats');
+
+        yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('Voir les plats', 'fas fa-eye', Plat::class),
+            MenuItem::linkToCrud('Ajouter un plat', 'fas fa-plus', Plat::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Modifier un plat', 'fas fa-plus', Plat::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Supprimer un plat', 'fas fa-plus', Plat::class)->setAction(Crud::PAGE_NEW),
+        ]);
+
+        yield MenuItem::section('Commandes');
+
+        yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('Voir les commandes', 'fas fa-eye', Commande::class),
+            MenuItem::linkToCrud('Modifier une Commande', 'fas fa-plus', Commande::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Supprimer une Commande', 'fas fa-plus', Commande::class)->setAction(Crud::PAGE_NEW),
+        ]);
+
+        yield MenuItem::section('Utilisateurs');
+
+        yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('Voir les utilisateurs', 'fas fa-eye', Utilisateur::class),
+            MenuItem::linkToCrud('Ajouter un utilisateur', 'fas fa-plus', Utilisateur::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Modifier un utilisateur', 'fas fa-plus', Utilisateur::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Supprimer un utilisateur', 'fas fa-plus', Utilisateur::class)->setAction(Crud::PAGE_NEW),
+        ]);
+    }
+}
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -14,11 +14,12 @@ class ComposerStaticInit1581de4d41a941a436bdac5ff8bebd15
         '8825ede83f2f289127722d4e842cf7e8' => __DIR__ . '/..' . '/symfony/polyfill-intl-grapheme/bootstrap.php',
         'b6b991a57620e2fb6b2f66f03fe9ddc2' => __DIR__ . '/..' . '/symfony/string/Resources/functions.php',
         'f598d06aa772fa33d905e87be6398fb1' => __DIR__ . '/..' . '/symfony/polyfill-intl-idn/bootstrap.php',
-        '6124b4c8570aa390c21fafd04a26c69f' => __DIR__ . '/..' . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
         '6a47392539ca2329373e0d33e1dba053' => __DIR__ . '/..' . '/symfony/polyfill-intl-icu/bootstrap.php',
+        '09f6b20656683369174dd6fa83b7e5fb' => __DIR__ . '/..' . '/symfony/polyfill-uuid/bootstrap.php',
+        '6124b4c8570aa390c21fafd04a26c69f' => __DIR__ . '/..' . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
+        'a1105708a18b76903365ca1c4aa61b02' => __DIR__ . '/..' . '/symfony/translation/Resources/functions.php',
         'ec07570ca5a812141189b1fa81503674' => __DIR__ . '/..' . '/phpunit/phpunit/src/Framework/Assert/Functions.php',
         '92c8763cd6170fce6fcfe7e26b4e8c10' => __DIR__ . '/..' . '/symfony/phpunit-bridge/bootstrap.php',
-        'a1105708a18b76903365ca1c4aa61b02' => __DIR__ . '/..' . '/symfony/translation/Resources/functions.php',
     );
 
     public static $prefixLengthsPsr4 = array (
@@ -38,6 +39,7 @@ class ComposerStaticInit1581de4d41a941a436bdac5ff8bebd15
         'S' => 
         array (
             'Symfony\\Runtime\\Symfony\\Component\\' => 34,
+            'Symfony\\Polyfill\\Uuid\\' => 22,
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Polyfill\\Intl\\Normalizer\\' => 33,
             'Symfony\\Polyfill\\Intl\\Idn\\' => 26,
@@ -54,6 +56,7 @@ class ComposerStaticInit1581de4d41a941a436bdac5ff8bebd15
             'Symfony\\Component\\VarExporter\\' => 30,
             'Symfony\\Component\\VarDumper\\' => 28,
             'Symfony\\Component\\Validator\\' => 28,
+            'Symfony\\Component\\Uid\\' => 22,
             'Symfony\\Component\\Translation\\' => 30,
             'Symfony\\Component\\String\\' => 25,
             'Symfony\\Component\\Stopwatch\\' => 28,
@@ -123,6 +126,7 @@ class ComposerStaticInit1581de4d41a941a436bdac5ff8bebd15
         'E' => 
         array (
             'Egulias\\EmailValidator\\' => 23,
+            'EasyCorp\\Bundle\\EasyAdminBundle\\' => 32,
         ),
         'D' => 
         array (
@@ -174,6 +178,10 @@ class ComposerStaticInit1581de4d41a941a436bdac5ff8bebd15
         'Symfony\\Runtime\\Symfony\\Component\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/runtime/Internal',
+        ),
+        'Symfony\\Polyfill\\Uuid\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-uuid',
         ),
         'Symfony\\Polyfill\\Mbstring\\' => 
         array (
@@ -238,6 +246,10 @@ class ComposerStaticInit1581de4d41a941a436bdac5ff8bebd15
         'Symfony\\Component\\Validator\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/validator',
+        ),
+        'Symfony\\Component\\Uid\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/uid',
         ),
         'Symfony\\Component\\Translation\\' => 
         array (
@@ -478,6 +490,10 @@ class ComposerStaticInit1581de4d41a941a436bdac5ff8bebd15
         'Egulias\\EmailValidator\\' => 
         array (
             0 => __DIR__ . '/..' . '/egulias/email-validator/src',
+        ),
+        'EasyCorp\\Bundle\\EasyAdminBundle\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/easycorp/easyadmin-bundle/src',
         ),
         'Doctrine\\SqlFormatter\\' => 
         array (
